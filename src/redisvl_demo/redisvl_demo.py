443b08bf8bd566deee9203cfa91733ca559aeb3c
savynorem <savannah.norem@redis.com>
2024-10-17 11:46:11 -0400

    refactor data load, add test file for simple vector search

--- src/redisvl_demo/redisvl_demo.py
+++ src/redisvl_demo/redisvl_demo.py
@@ -1,22 +1,26 @@
 import redis
 from load_anime_data import DataLoader
-from run_demo import Demo
+from run_search_ui import SearchUI
 from sys import argv
-
+import argparse
+parser = argparse.ArgumentParser(exit_on_error=False)
 
 def main(load_flag = True, demo_flag = True):
     # This function initializes the Redis connection and runs the data loading and demo functions
-    r = redis.Redis(host='localhost', port=6379, db=0)
+    r = redis.Redis(host='localhost', port=6379, db=0) # make host & port env variable
     if load_flag:
         loader = DataLoader(r)
-        loader.parse_data()
         loader.load_data()
     if demo_flag:
-        demo = Demo(r)
-        demo.run_demo()
+        search_ui = SearchUI(r)
+        search_ui.run_search_ui()
 
 if __name__ == "__main__":
-    demo_flag, load_flag = True, True
+    #parser.add_argument('--load-data', '-l', nargs=2) # if data path provided, load that file, don't parse it (should this include the schema?)
+    #parser.add_argument('--start', '-s')
+
+    load_flag = True
+    demo_flag = True
     if len(argv) == 2:
         if argv[1] == 'loadonly':
             demo_flag = False
